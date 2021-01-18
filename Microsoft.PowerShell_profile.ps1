a4d51282443456019c77bd9369384456c9b14b00
Kevin Crouch <PsychoData91@gmail.com>
2021-01-18 07:27:28 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,21 @@
 #Windows Powershell 5.1 - AllUsersCurrentHost 
 #C:\Windows\System32\WindowsPowerShell\v1.0\Microsoft.PowerShell_profile.ps1
 
+$requiredModules = @{
+ 'Posh-Git' = $null
+ #Module
+ #RequiredVersion 
+ #PreReleaseRequired?
+}
+
+
+if ( -not [bool](Get-Module posh-git)) {
+    Install-Module Posh-Git -Confirm:$true -scope AllUsers -AllowPrerelease 
+} else { 
+    Write-Verbose "Module Posh-Git already found"
+}
+
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '$(if([bool](Get-GitStatus)){"`n"})'
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory  = $true
+
