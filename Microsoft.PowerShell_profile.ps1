3f3ef3ff227ee8212c012858079067c500dbe80d
Kevin Crouch <PsychoData91@gmail.com>
2021-01-18 04:07:10 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-﻿
+#Windows Powershell 5.1 - AllUsersCurrentHost 
+#C:\Windows\System32\WindowsPowerShell\v1.0\Microsoft.PowerShell_profile.ps1
+
 Import-Module posh-git
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '$(if([bool](Get-GitStatus)){"`n"})'
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory  = $true
